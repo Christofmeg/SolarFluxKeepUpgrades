9f31db7ed4fa411f4bca1c1deaebea420300a853
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2026-05-25 08:52:51 +0200

    dev

--- src/main/java/com/christofmeg/solarfluxkeepupgrades/SolarFluxKeepUpgrades.java
+++ src/main/java/com/christofmeg/solarfluxkeepupgrades/SolarFluxKeepUpgrades.java
@@ -9,29 +9,26 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 import java.util.Optional;
 
-@SuppressWarnings("removal")
+
 @Mod(SolarFluxKeepUpgrades.MOD_ID)
 public class SolarFluxKeepUpgrades {
 
     public static final String MOD_ID = "solarfluxkeepupgrades";
 
-    public SolarFluxKeepUpgrades() {
-        MinecraftForge.EVENT_BUS.register(this);
-        RecipeRegistry.init(FMLJavaModLoadingContext.get().getModEventBus());
+    public SolarFluxKeepUpgrades(IEventBus modBus) {
+        modBus.register(this);
+        RecipeRegistry.init(modBus);
     }
 
-    //TODO integration to https://www.curseforge.com/minecraft/mc-mods/re-avaritia
-    //TODO integration to https://www.curseforge.com/minecraft/mc-mods/the-twilight-forest
-
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SolarFluxKeepUpgrades.MOD_ID)
+    @EventBusSubscriber(modid = SolarFluxKeepUpgrades.MOD_ID)
     public static class Common {
         @SubscribeEvent
         public static void onCratedEvent(PlayerEvent.ItemCraftedEvent event) {
